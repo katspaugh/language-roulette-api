721348d2ecd1d251d7263177ee34fc6218d5e301
katspaugh <ivan@mycs.com>
2017-08-13 00:42:26 +0200

    rooms list

--- handler.js
+++ handler.js
@@ -1,26 +1,30 @@
 'use strict';
 
-var AccessToken = require('twilio').jwt.AccessToken;
-var randomName = require('./randomname');
+const Twilio = require('twilio');
+const randomName = require('./randomname');
 
-module.exports.token = (event, context, callback) => {
-  const respond = (err, data) => {
-    const response = {
-      statusCode: err ? 500 : 200,
-      headers: {
-        'Access-Control-Allow-Origin' : '*'
-      },
-      body: JSON.stringify(data)
-    };
+function corsResponse(callback, err, data) {
+  if (err) console.log(err);
 
-    callback(null, response);
+  const response = {
+    statusCode: err ? 500 : 200,
+    headers: {
+      'Access-Control-Allow-Origin' : '*'
+    },
+    body: JSON.stringify(data)
   };
 
-  var identity = randomName();
+  callback(null, response);
+}
+
+module.exports.token = (event, context, callback) => {
+  const respond = corsResponse.bind(null, callback);
+  const AccessToken = Twilio.jwt.AccessToken;
+  const identity = randomName();
 
   // Create an access token which we will sign and return to the client,
   // containing the grant we just created.
-  var token = new AccessToken(
+  const token = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_KEY,
     process.env.TWILIO_API_SECRET
@@ -30,7 +34,7 @@ module.exports.token = (event, context, callback) => {
   token.identity = identity;
 
   // Grant the access token Twilio Video capabilities.
-  var grant = new AccessToken.VideoGrant();
+  const grant = new AccessToken.VideoGrant();
   token.addGrant(grant);
 
   // Serialize the token to a JWT string and include it in a JSON response.
@@ -38,4 +42,30 @@ module.exports.token = (event, context, callback) => {
     identity: identity,
     token: token.toJwt()
   });
+};
+
+module.exports.rooms = (event, context, callback) => {
+  const respond = corsResponse.bind(null, callback);
+
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created.
+  const client = new Twilio(
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET,
+    {
+      accountSid: process.env.TWILIO_ACCOUNT_SID
+    }
+  );
+
+  client.video.rooms.list({
+    status: 'in-progress'
+  })
+    .then(rooms => {
+      respond(null, rooms.map(room => ({
+        dateCreated: room.dateCreated,
+        dateUpdated: room.dateUpdated,
+        uniqueName: room.uniqueName
+      })));
+    })
+    .catch(err => respond(err));
 };
